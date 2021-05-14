631c53058ed006b52585449e6a09dee023175f45
Neel <neelusenthil1996@gmail.com>
2021-05-15 00:43:04 +0530

    Update lint and format config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: "@react-native-community",
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  trailingComma: "all",
+  arrowParens: "avoid",
+  semi: true,
 };
