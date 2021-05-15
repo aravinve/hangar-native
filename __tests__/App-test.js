8a93a6360cb4a0996a4d6b02fea9d3c2572821c6
Neel <neelusenthil1996@gmail.com>
2021-05-15 14:02:17 +0530

    Update test for App

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,10 +1,12 @@
 import "react-native";
 import React from "react";
 import App from "../App";
-
-// Note: test renderer must be required after react-native.
 import renderer from "react-test-renderer";
 
 it("renders correctly", () => {
-  renderer.create(<App />);
+  const renderedApp = renderer.create(<App />);
+  const { children } = renderedApp.toJSON();
+
+  expect(renderedApp).toBeTruthy();
+  expect(children.length).toBeGreaterThan(0);
 });
