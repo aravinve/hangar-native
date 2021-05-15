9930d1aa74696f9bee7a8b46c1a13886e28d26ac
Neel <neelusenthil1996@gmail.com>
2021-05-15 13:28:36 +0530

    Update add name for the project

--- android/app/src/main/java/com/hanger/MainActivity.java
+++ android/app/src/main/java/com/hanger/MainActivity.java
@@ -1,4 +1,4 @@
-package com.hangermobile;
+package com.hanger;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "HangerMobile";
+    return "Hanger";
   }
 }
