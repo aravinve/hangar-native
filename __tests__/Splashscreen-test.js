b417738d8ca4f29a8d458d149c9d15b383964799
Neel <neelusenthil1996@gmail.com>
2021-05-16 13:01:24 +0530

    Improve test cases for splash screen

--- __tests__/Splashscreen-test.js
+++ __tests__/Splashscreen-test.js
@@ -6,8 +6,12 @@ import Splashscreen from "../src/components/Splashscreen";
 it("renders Splashscreen with logo image", () => {
   const renderedImage = render(<Splashscreen />).getByTestId("logoImage");
   const renderedLabel = render(<Splashscreen />).getByTestId("logoLabel");
+  const testImageSource = renderedImage.props.source.testUri;
+  const testLabelText = renderedLabel.children[0];
 
   expect(renderedImage).toBeTruthy();
   expect(renderedLabel).toBeTruthy();
-  expect(renderedLabel.children[0]).toBe("Hangar");
+
+  expect(testImageSource.includes("logo.png")).toBe(true);
+  expect(testLabelText).toBe("Hangar");
 });
