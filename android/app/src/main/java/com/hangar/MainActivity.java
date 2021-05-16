ed267b3de346bd9ef66e121727b03fba37bfd008
Neel <neelusenthil1996@gmail.com>
2021-05-16 13:01:01 +0530

    Fix typo in app name by rebuilding native bundles

--- android/app/src/main/java/com/hangar/MainActivity.java
+++ android/app/src/main/java/com/hangar/MainActivity.java
@@ -1,4 +1,4 @@
-package com.hanger;
+package com.hangar;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Hanger";
+    return "Hangar";
   }
 }
--- android/app/src/main/java/com/hangar/MainApplication.java
+++ android/app/src/main/java/com/hangar/MainApplication.java
@@ -1,4 +1,4 @@
-package com.hanger;
+package com.hangar;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.hanger.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.hangar.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
