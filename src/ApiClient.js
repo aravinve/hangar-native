113dd930a46db6735163fb89931589dfa3161395
Neel <neelusenthil1996@gmail.com>
2021-05-23 21:33:39 +0530

    Add common axios client

---
+++ src/ApiClient.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const AxiosClient = axios.create({
+  baseURL: getBaseUrl,
+});
+
+const getBaseUrl = () => {
+  switch (process.env.HANGAR_ENVIRONMENT) {
+    case "local":
+      return "http://localhost:5000/";
+    case "dev":
+      return "";
+  }
+};
