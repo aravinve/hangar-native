317a2878df1419ab7b83ce3ce5c7a5433974c2df
Neel <neelusenthil1996@gmail.com>
2021-06-04 23:58:05 +0530

    Add module path alias for enabling non-relative imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          components: "./src/components",
+          assets: "./src/assets",
+          screens: "./src/screens",
+          config: "./src/config",
+          ApiClient: "./src/ApiClient.js",
+        },
+      },
+    ],
+  ],
 };
